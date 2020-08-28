216a291370630c0eae01ca4ac3587139568ebbd3
Victor Henrique <victor.henrique5800@gmail.com>
2020-08-28 14:34:43 -0300

    First commit

---
+++ core/src/main/java/Main.java
@@ -0,0 +1,16 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        log.info("Program Initiating");
+        try(ConfigurableApplicationContext context =
+                    new ClassPathXmlApplicationContext("beans.xml")){
+            NumberGeneratorImpl generator = context.getBean("numberGenerator", NumberGeneratorImpl.class);
+            System.out.println(generator.next());
+        }
+    }
+}
